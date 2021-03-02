76840178a28e92f15fc06817634b22b3205b73cf
velibor10 <veliborzdravkovic1010@gmail.com>
2021-03-02 19:37:36 +0100

    Initial commit

---
+++ Week2Practice/conditions.js
@@ -0,0 +1,127 @@
+/*Task 1. Write a conditional statement to find the sign of product of three numbers. Display the
+result in the console with the specified sign.
+Sample numbers: 3, -7, 2
+Output: The sign is - */
+
+var a = 3;
+var b = -7;
+var c = 2;
+var product = a * b * c;
+if (a * b * c > 0) {
+    console.log("+")
+}else {
+    console.log("-")
+}
+
+/* Task 2. Write a conditional statement to find the largest of five numbers. Display the result in the
+console.
+Sample numbers: -5, -2, -6, 0, -1
+Output: 0 */
+
+var a = -5;
+var b = -2;
+var c = -6;
+var d = 0;
+var e = -1;
+
+if (a > d) {
+    console.log(a);
+} else if (b > d) {
+console.log(b);
+} else if (c > d) {
+    console.log(c);
+} else if (e > d) {
+    console.log(e);
+} else {
+    console.log(d);
+}
+
+/*Task 3. Write a conditional statement to print three numbers as sorted number list.
+Sample numbers : 0, -1, 4
+Output : 4, 0, -1 */
+
+var a = 0;
+var b = -1; 
+var c = 4;
+if (a > b && a > c && b > c) {
+    console.log(a, b , c);
+} else if (b > a && b > c && a > c) {
+    console.log(b, a, c);
+} else if (c > a && c > b && a > b) {
+    console.log(c, a, b)
+}else {
+    console.log("wrong")
+}
+
+/* Task 4. Write a program to check if the variable is a number and if it’s a number, check if it is
+divisible by 2. If it is, print the result, if not, show “X”.
+Sample input: 10 Sample input: 7
+Output: 10 / 2 = 5 Output: X */
+
+var a = 10;
+var result = a / 2;
+if (a % 2 === 0) {
+    console.log(result);
+} else if (a / 2 !== 0 ) {
+    console.log("X");
+}
+
+/* Task 5. Write a program that compares two numbers and displays the larger. Display the result in
+the console. */
+
+var a = 1000;
+var b = 10;
+if (a > b) {
+    console.log(a);
+} else if (b > a) {
+    console.log(b);
+} else {
+    console.log("a = b");
+}
+
+/* Task 6. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+Formula : F = (9*C/5) + 32 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
+Sample Input: 60°C
+Output : 60°C is 140 °F */
+
+var c = 60;
+var f = (9 * c / 5) + 32
+console.log(f); 
+
+/* Task 7. Write a JavaScript program to get the difference between a given number and 13, if
+the number is greater than 13 return double difference between that number and 13.
+Sample Input: 11 Sample Input: 32
+Output : 2 Output : 38 */
+
+var a = 32;
+if (a < 13) {
+    console.log(13 - a);
+} else if (a > 13) {
+    console.log((a - 13) * 2);
+}
+
+/* Task 8. Write a JavaScript program to compute the sum of the two given integers. If the two
+values are same, then returns triple their sum.
+Sample Input: 12,5 Sample Input: 8,8
+Output : 17 Output : 48 */
+
+/* Task 9. Write a JavaScript program to check two given numbers and print “true” if one of
+the number is 50 or if their sum is 50.
+Sample Input: 5,54 Sample Input: 6,50 Sample Input: 40,10
+Output : - Output : true Output : true */
+
+/* Task 10. Write a JavaScript program to check a given integer is within 20 of 100 or 400,
+and print range in which number belongs.
+Sample Input: 13 Sample Input: 34 Sample Input: 256
+Output : - Output : 20 ⇔ 100 Output : 100 ⇔ 400 */ 
+ 
+var a = 13;
+if (a < 20) {
+    console.log("-");
+} else if (a >= 20 && a <= 100) {
+    console.log("20 <==>");
+} else if (a >= 100 && a <= 400) {
+    console.log("100 <=> 400");
+} else ("error");
+
+
---
+++ Week2Practice/variables.js
@@ -0,0 +1,144 @@
+/*Variable declaration and initialization
+1. Create several variables using different letter cases and beginning characters. Check if they
+still exist if you refer to them in different case - does case matter here? Try to declare a variable
+starting with number as the first character.*/
+
+var a = 10;
+var word = "rec";
+var x = a + a;
+var Bulian = true;
+var $888 = 888;
+
+//2. Save a string (text) describing your current mood in a variable and print it out in console//
+
+var tekst = "Jos sam pospan i pije mi se kafa";
+console.log(tekst);
+
+//3. Feel free to play around in the console and get familiar with it.//
+
+console.log(typeof $888);
+
+/*Arithmetic operators
+4. Play around in the console: add, subtract, multiply and divide some numbers. Use numbers
+stored in variables you created as well. Think about expressions: 3-2-31, 3*5-7/3, 4/2-1.14*3.
+Why do we get these results?*/
+
+var broj = 5;
+var brojevi = 12;
+var vrednost = 25;
+var velicina = 11;
+
+c = broj - brojevi - vrednost;
+d = broj * brojevi - vrednost / velicina;
+e = broj / brojevi - vrednost * velicina;
+console.log(c,d,e)
+
+//5. How many grams weight 1.2kg of bananas?//
+
+var weightInKg = 1.2;
+var weightInGr = weightInKg * 1000;
+console.log(weightInGr);
+
+/*6. How many extra seconds we wait for our friend Anna who is being late for 5 minutes? What
+about Tom who is being late half an hour? It is hard to be on time these days…*/
+
+var waitmin1 = 5;
+var waitmin2 = 30;
+var waitAnna = waitmin1 * 60;
+var waitTom = waitmin2 * 60;
+console.log("We will wait Anna" + " " + waitAnna + " " + "seconds");
+console.log("We will wait Tom" + " " + waitTom + " " + "seconds");
+
+/*7. How many MB of additional memory we have if we buy a 4GB USB stick? How many news
+articles each in size 98KB can fit in it?*/
+
+var UsbMemory = 4;
+var adMemory =  UsbMemory * 1000;
+var numberOfArticles = adMemory * 1000 / 98;
+console.log(adMemory);
+console.log(numberOfArticles);
+
+//8. Do you know any other way to write a = a * 2? How about: b = b + 3; c = c / 5; d = d - 7?//
+
+var a = 2;
+var result = a*=2;
+console.log(result);
+var b = 5;
+var result1 = b+=3;
+console.log(result1);
+var c = 10;
+var result2 = c/=5;
+console.log(result2);
+var d = 4;
+var result3 = d-=7;
+console.log(result3);
+
+/* 9. Create the following variables:
+- a variable containing your name,
+- a variable containing your age,
+- a variable describing if you have a cat or not.
+* Each variable has to start with a different type of character.
+Print them all out in the console and check their types.*/
+
+var name = "Velibor";
+var age = 36;
+var iHaveACat = false ;
+console.log(name,age,iHaveACat);
+console.log(typeof name,typeof age,typeof iHaveACat);
+
+/* 11. Check which type are these values:
+- “number”
+- true
+- null
+- “false”
+- 56*/
+
+var a = "number";
+var b = true;
+var c = null;
+var d = "false";
+var e = 56;
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e);
+
+/*Logical operators
+12. Check how good you are at guessing the Boolean equivalent of different values using the
+console. Do you remember how to check for a Boolean equivalent of a value?*/
+
+var a = "";
+var b = null;
+var c = undefined;
+var d = 0;
+var e = NaN;
+var f = false;
+console.log(!!a, !! b, !!c, !!d, !!e, !!f);
+console.log(!a, !b, !c, !d, !e, !f);
+
+/* 13. Use the console to check the results you think should go in the following table:
+
+p q                         p && q       p || q
+true true                   true         true                
+true false                  true         false
+false true                  true         false
+false false                 false        false   */
+
+var p = true;
+var q = true;
+console.log(p && q);
+
+/* 14. If the variable age stores the information about user’s age, how we can check whether it
+contains a correct value? Write expressions for checking:
+● Is age negative value
+● Is age greater than 120*/
+
+var age = 50;
+var 
+console/log(age >= 0)
+
+/* 15. Let’s say there are speed limitations on a motorway from 60 to 120 kilometers per hour. If
+we store the current speed value in the variable speed. Write an expression which will check if
+we are driving drive safely or not? (true if we are driving safe and false if not)*/
+
+var speed = 200;
+var checkSpeed = speed >= 60 && speed <= 120;
+console.log(checkSpeed);
+
