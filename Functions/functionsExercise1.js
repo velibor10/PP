412933bd6eb5c1fd10f29c1fb751fcaa92b5808e
velibor10 <veliborzdravkovic1010@gmail.com>
2021-03-30 20:28:38 +0200

    Added Function Exercise1 and Exercise2

---
+++ Functions/functionsExercise1.js
@@ -0,0 +1,196 @@
+// 1. Write a program that calculates the maximum of two given numbers.
+
+function maximum (a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(maximum(100, 23));
+
+
+// 2. Write a program that checks if a given number is odd.
+
+function oddNumber (x) {
+    if (x % 2 === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(oddNumber(13));
+
+
+// 3. Write a program that checks if a given number is a three digit long number.
+
+function threeDigitLongNumber (y) {
+    if (y > 99 && y < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(threeDigitLongNumber(999));
+
+
+// 4. Write a program that calculates an arithmetic mean of four numbers.
+
+function arMean (a, b, c, d) {
+    return (a + b + c + d) / 2;
+}
+
+console.log(arMean(1, 2, 3, 4));
+
+
+/* 5. Write a program that draws a square of a given size. For example, if the size of square
+is 5 the program should draw:
+*****
+*   *
+*   *
+*   *
+*****  */
+
+function drawSquare (number) {
+    let empty = "";
+    let star = "*";
+    let space = " ";
+    let newLine = "\n";
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            if (i === 0 || i === number - 1 || j === 0 || j === number - 1) {
+                empty += star;
+            } else {
+                empty += space;
+            }
+        }
+        empty += newLine;
+    }
+    return empty;
+}
+
+console.log(drawSquare(7));
+
+
+/* 6. Write a program that draws a horizontal chart representing three given values. For
+example, if values are 5, 3, and 7, the program should draw:
+
+* * * * *
+* * *
+* * * * * * *  */
+
+function horizontalChart (a, b, c) {
+    let empty = "";
+    let star = "* ";
+    let newLine = "\n";
+    for (let i = 0; i < a; i++) {
+        empty += star;
+    }
+        empty += newLine;
+    for (let j = 0; j < b; j++) {
+        empty += star;
+    }
+        empty += newLine; 
+    for (let k = 0; k < c; k++) {
+        empty += star;
+    }
+        empty += newLine;
+    return empty; 
+}
+
+console.log(horizontalChart(5, 3, 7));
+
+
+// 7. Write a program that calculates a number of digits of a given number.
+
+function numberOfDigits (number) {
+    let empty = "";
+    for (let i = 0; i < number.length; i++ ) {
+        empty [i]= number[i];
+    }
+    empty += number; 
+    return empty.length;
+}
+
+console.log(numberOfDigits(1234));
+
+
+/* 8. Write a program that calculates a number of appearances of a given number in a given
+array.
+Inputs: a = [2, 4, 7, 8, 7, 7, 1], e = 7
+Result: 3   */
+
+function numberOfAppearances (array, x) {
+    let appearances = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === x) {
+            appearances++;
+        }
+    }
+    return appearances;
+}
+
+console.log(numberOfAppearances([2, 4, 7, 8, 7, 7, 1], 7));
+
+
+// 9. Write a program that calculates the sum of odd elements of a given array.
+
+function calculates (array) {
+    let sume = 0;
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] % 2 != 0) {
+        sume += array[i];
+        }
+    }
+    return sume;
+}
+
+console.log(calculates([2, 4, 7, 8, 7, 7, 1, 7, 2, 10 ,10]));
+
+
+/* 10. Write a program that calculates the number of appearances of a letter a in a given string.
+Modify the program so it calculates the number of both letters a and A.  *//
+
+function numberOfAppearances (string) {
+    let appear = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "a") {
+            appear ++;
+        }
+    }
+    return appear;
+}
+
+console.log(numberOfAppearances("alamanaaAAAaA"));
+
+
+function numberOfAppearances (string) {
+    let appear = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "a" || string[i] === "A") {
+            appear ++;
+        }
+    }
+    return appear;
+}
+
+console.log(numberOfAppearances("alamanaaAAAaA"));
+
+
+/* 11. Write a program that concatenates a given string given number of times. For example, if
+“abc” and 4 are given values, the program prints out abcabcabcabc.  */
+
+function concatenatesString (string, x) {
+    let empty = "";
+    for (let i = 0; i < string.length; i++) {
+        for (i = 0; i < x; i++) {
+            empty += string;
+        }
+    }
+    return empty;
+}
+
+console.log(concatenatesString("abc", 2));
---
+++ Functions/functionsExercise2.js
@@ -0,0 +1,226 @@
+/* 1. Write a function to check wheter the `input` is a string or not.
+
+"My random string" -> true
+12 -> false */
+
+function isString(string) {
+    return typeof string === "string";
+}
+
+console.log(isString(12));
+
+
+/* 2. Write a function to check wheter a string is blank or not.
+
+"My random string " -> false
+" " -> true
+12 - > false
+false -> false */
+
+function blankString(input) {
+    if (input === " ") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(blankString(false));
+
+
+/* 3. Write a function that concatenates a given string n times (default is 1).
+"Ha" -> "Ha"
+"Ha" 3 -> "HaHaHa" */
+
+function concatenates(string) {
+    let empty = "";
+    let n = 3;
+    for (let i = 0; i < string.length; i ++) {
+        empty += string[i];
+    }
+    for (j = 1; j < n; j++) {
+        empty += string;
+    }
+    return empty;
+}
+
+console.log(concatenates("Ha"));
+
+
+/* 4. 4. Write a function to count the number of letter occurrences in a string.
+"My random string", "n" -> 2. */
+
+function countNumberOfLetters (string, letter) {
+    let sume = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === letter) {
+            sume += 1;
+        }
+    }
+    return sume;
+}
+
+console.log(countNumberOfLetters("My random string", "n"));
+
+
+/* 5. Write a function to find the position of the first occurrence of a character in a string. The
+result should be the position of character. If there are no occurrences of the character, the
+function should return -1. */
+
+function firstOccurrenceOfACharacter(string, position) {
+    let result = -1;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === position) {
+            result = i + 1;
+            break;
+        } 
+    }
+    return result;
+}
+
+console.log(firstOccurrenceOfACharacter("occupation", "c"));
+
+
+/* 6. Write a function to find the position of the last occurrence of a character in a string. The
+result should be in human numeration form. If there are no occurrences of the character,
+function should return -1. */
+
+function lastOccurrenceOfACharacter (string, position) {
+    let result = -1;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === position) {
+            result = i + 1;
+        }
+    }
+    return result;
+}
+
+console.log(lastOccurrenceOfACharacter("repetition", "t"));
+
+
+/* 7. Write a function to convert string into an array. Space in a string should be represented as
+“null” in new array.
+
+"My random string" -> ["M", "y", null, "r", "a"]
+"Random" -> ["R", "a", "n", "d", "o", "m"] */
+
+function convertStringIntoArray (string) {
+    let empty = [];
+    for (let i = 0; i < string.length; i ++) {
+        if (string [i] === " ") {
+            empty[i] = null;
+        } else {
+            empty [i] = string[i];  
+        }
+    }
+    return empty;
+}
+
+console.log(convertStringIntoArray("Random"));
+
+
+
+/* 8. Write a function that accepts a number as a parameter and checks if the number is prime or
+not.
+Note: A prime number (or a prime) is a natural number greater than 1 that has no positive
+divisors other than 1 and itself. */
+
+function isNumberPrime (x) {
+    let counter = 0;
+    for (i = 1; i <= x; i++) {
+        if (x % i === 0) {
+        counter++;
+        }
+    }        
+    if (counter > 2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+ console.log(isNumberPrime(12));
+
+/* 9. Write a function that replaces spaces in a string with provided separator. If separator is not
+provided, use “-” (dash) as the default separator.
+
+"My random string", " " -> "My_random_string"
+"My random string", "+" -> "My+random+string"
+"My random string" -> "My-random-string", */
+
+function replaceSpaces (string, separator) {
+    let empty = "";
+    separator = separator || "-";
+    for (i = 0; i < string.length; i++) {
+        if (string[i] === " ") {
+            empty += separator;
+        } else {
+            empty += string[i];
+        }
+    }
+    return empty;
+}
+
+console.log(replaceSpaces("My random string"));
+
+
+// 10. Write a function to get the first n characters and add “...” at the end of newly created string.
+
+function nCharacters (string, n) {
+    let empty = "";
+    for (let i = 0; i < n; i++) {
+        empty += string[i]; 
+    }
+    return empty += "...";
+}
+
+console.log(nCharacters("Velibor", 4));
+
+
+/* 11. Write a function that converts an array of strings into an array of numbers. Filter out all
+non-numeric values.
+
+["1", "21", undefined, "42", "1e+3", Infinity] -> [1, 21, 42, 1000] */
+
+
+function arrToString (array) {
+    let emptyArr = [];
+    for (let i = 0; i < array.length; i++) {
+        let convert  = parseFloat(array[i]);
+        if (isFinite(convert)) {
+            emptyArr[emptyArr.length] = convert;
+        }
+    }
+        return emptyArr;
+}
+
+console.log(arrToString(["1", "21", undefined, "42", "1e+3", Infinity]));
+
+
+/* 12. Write a function to calculate how many years there are left until retirement based on the
+year of birth. Retirement for men is at age of 65 and for women at age of 60. If someone is
+already retired, a proper message should be displayed. */
+
+function retirement (currentYear , yearOfBirth, gender) {
+    let yearsToRetire = 0;
+    if (gender = "male" && currentYear - yearOfBirth < 65) {
+        yearsToRetire = 65 - (currentYear - yearOfBirth);
+    } else {
+        yearsToRetire = "Already retired";
+    }
+    if (gender = "female" && currentYear - yearOfBirth < 60) {
+        yearsToRetire = 60 - (currentYear - yearOfBirth);;
+    } else {
+        yearsToRetire = "Already retired"; 
+    }
+    return yearsToRetire;
+}
+
+console.log(retirement(2022, 1963, "female"));
+
+/* 13. Write a function to humanize a number (formats a number to a human-readable string) with
+the correct suffix such as 1st, 2nd, 3rd or 4th.
+
+1 -> 1st
+11 -> 11th; */
+
+
