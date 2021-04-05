6dfa33fd783a731ba7b3d44a97a168e2ee155b92
velibor10 <veliborzdravkovic1010@gmail.com>
2021-04-05 13:37:37 +0200

    Deleted unnecessery text after task 8

--- LoopsExercises/loopsExercises.js
+++ LoopsExercises/loopsExercises.js
@@ -150,11 +150,6 @@ for (var a = 1; a <= 100; a++) {
 }
 
 
-/*7. Write a program that intertwines two arrays. You can assume the arrays are of the same
-length.
-Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
-Output array: [4, 3, 5, 8, 6, 11, 2, 9] */
-
 
 
 
