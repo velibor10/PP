75cd1b42f1b77088fdbebcc63402300962d3881f
velibor10 <veliborzdravkovic1010@gmail.com>
2021-06-14 03:17:59 +0200

    Finished both tasks old way of inheritance exercise

--- Objects/MethodsInheritanceExercises/inheritance.js
+++ Objects/MethodsInheritanceExercises/inheritance.js
@@ -6,21 +6,31 @@ Employees and Managers
 Exercise done with old inheritance model using Constructor classes, call and this from parent class
 
 1. Create constructor functions with properties representing the following:
+
 Person: name, surname
+
 Employee: inherits Person and has job and salary
+
 Developer: inherits from Employee and has specialization
+
 Manager: inherits from Employee and has department
 
 2. All developers should inherit method:
+
 getSpecialization which prints out the name of the specialization
 
 3. All managers should inherit methods:
+
 getDepartment which prints out the name of the department
+
 changeDepartment which sets the department value on the given value
 
 4. All employees should inherit methods:
+
 getData which returns the name, surname and salary 
+
 getSalary which prints out the salary
+
 increaseSalary which increases the salary by 10% */
 
 function Person (name, surname) {
@@ -79,7 +89,7 @@ Employee.prototype.getSalary = function () {
     console.log(this.salary);
 }
 
-Employee.prototype.increaseSalary = function (salary) {
+Employee.prototype.increaseSalary = function () {
     
     return this.salary += (this.salary * 0.1);
 }
@@ -144,6 +154,10 @@ Manager.prototype.changeDepartment = function (department) {
 
 // Testing
 
+console.log("\n");
+console.log("Task1 Employees and Managers");
+console.log("\n");
+
  let pera = new Person ("pera", "peric");
  let laza = new Employee ("laza", "lazic", "supervisor",100000);
  let mika = new Developer ("mika", "mikic", "developer", 110000, "JS");
@@ -176,10 +190,170 @@ console.log(jova.increaseSalary());
 
 
 
+/* Applications:
+
+1. Create constructor functions with properties representing the following:
+
+WebApp: name, url, technologies, licence, stars
+
+MobileApp: name, platforms, licence, stars
+ 
+2. All web applications should inherit methods: 
+
+getData which prints out all the information
+
+reactBased which checks if one of the used technologies is React
+ 
+3. All mobile applications should inherit methods:
+
+getData which prints out all the informations
+
+forAndroid which checks if one of the platforms the application is developed for is Android
+
+4. Both web and mobile applications should inherit methods:
+
+isCCLicence  which checks if the licence of the application is CC (Creative Commons) 
+
+like which increases the number of stars by one
+
+showStars which prints out the number of stars */
+
+
+console.log("\n");
+console.log("Task1 Employees and Managers");
+console.log("\n");
 
 
 
+function Applications (name, licence, stars) {
+
+    if (!name) {
+        throw Error("Name must be defined");
+    }
+    if (!licence) {
+        throw Error("Licence must be defined");
+    }
+    if (!stars) {
+        throw Error("Stars must be defined");
+    }
+    if(typeof name !== "string") {
+        throw new Error("Name must be a text")
+    }
+    if(typeof licence !== "string") {
+        throw new Error("Licence must be a text")
+    }
+    if(typeof stars !== "number") {
+        throw new Error("Stars must be a number");
+    }
+
+    this.name = name;
+    this.licence = licence;
+    this.stars = stars;
+
+}
+
+Applications.prototype.isCCLicence = function () {
+    if (this.licence === "CC") {
+        console.log("Licence for this application is CC");
+    }
+    else {
+        console.log("Licence for this application is not CC");
+    }
+}
+
+Applications.prototype.like = function () {
+    console.log(this.stars = this.stars + 1);
+}
+
+Applications.prototype.showStars = function () {
+    console.log(this.stars);
+}
 
 
 
+function WebApp (name, licence, stars, url, technologies) {
+    Applications.call(this, name, licence, stars);
+
+    if (!url) {
+        throw Error("Url must be defined");
+    }
+    if (!technologies) {
+        throw Error("Technologies must be defined");
+    }
+    if(typeof url !== "string") {
+        throw new Error("Name must be a text")
+    }
+    if(typeof technologies !== "string") {
+        throw new Error("Licence must be a text")
+    }
+
+    this.url = url;
+    this.technologies = technologies;
+}
+
+WebApp.prototype = Object.create(Applications.prototype);
+WebApp.prototype.constructor = WebApp;
+
+WebApp.prototype.getData = function () {
+    console.log("NAME: " + this.name + ", URL: " + this.url + ", TECH: " + this.technologies + ", LICENCE: " + this.licence + ", STARS: " + this.stars);
+}
+
+WebApp.prototype.reactBased = function () {
+    if (this.technologies === "React") {
+        console.log("This web application is using React technology");
+    }
+    else {
+        console.log("This web application is not using React technology");
+    }
+}
+
+function MobileApp (name, licence, stars, platforms) {
+    Applications.call(this, name, licence, stars);
+
+    if (!platforms) {
+        throw Error("Platforms must be defined");
+    }
+    if (typeof platforms !== "string") {
+        throw new Error("Platforms must be a text");
+    }
+
+    this.platforms = platforms;
+}
+
+MobileApp.prototype = Object.create(Applications.prototype);
+MobileApp.prototype.constructor = MobileApp;
+
+MobileApp.prototype.getData = function () {
+    console.log("NAME: " + this.name + ", LICENCE: " + this.licence + ", STARS: " + this.stars + ", PLATFORMS: " + this.platforms);
+}
+
+MobileApp.prototype.forAndroid = function () {
+    if(this.platforms === "Android") {
+        console.log("This platform is developed for Android")
+    }
+    else {
+        console.log("This platform is not developed for Android");
+    }
+}
+
+
+
+// TESTING
+
+let application = new Applications("facebook", "CCC", 5)
+let webApp = new WebApp("instagram", "CCC", 4, "instagram.com", "Node");
+let mobileApp = new MobileApp("twitter", "CCCC", 3, "Android");
+
+console.log(application);
+console.log(webApp);
+console.log(mobileApp);
+
+webApp.getData();
+webApp.reactBased();
+mobileApp.getData();
+mobileApp.forAndroid();
+application.isCCLicence();
+application.like();
+application.showStars();
+application.like();
 
